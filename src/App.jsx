c77d424d0ae5a949ce937263f8e8a3b82ff3d419
Darkned07 <madaminovmuhammadyusuf806@gmail.com>
2024-04-19 18:13:55 +0500

    update portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import { Footer, Navbar } from "./components";
-import { About, Contact, Home, Projects, Resume } from "./pages";
+import { About, Contact, Home, Projects } from "./pages";
 
 const App = () => {
   return (
-    <main className='bg-slate-300/20'>
+    <main className="bg-slate-300/20">
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route
-            path='/*'
+            path="/*"
             element={
               <>
                 <Routes>
-                  <Route path='/about' element={<About />} />
-                  <Route path='/projects' element={<Projects />} />
-                  <Route path='/contact' element={<Contact />} />
-                  <Route path='/resume' element={<Resume />} />
-                  
+                  <Route path="/about" element={<About />} />
+                  <Route path="/projects" element={<Projects />} />
+                  <Route path="/contact" element={<Contact />} />
                 </Routes>
                 <Footer />
               </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,34 @@ import { logo } from "../assets/images";
 
 const Navbar = () => {
   return (
-    <header className='header'>
-      <NavLink to='/'>
-        <img src={logo} alt='logo' className='w-18 h-18 object-contain' />
+    <header className="header">
+      <NavLink to="/">
+        <img src={logo} alt="logo" className="w-18 h-18 object-contain" />
       </NavLink>
-      <nav className='flex text-lg gap-7 font-medium'>
-        <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? "text-blue-600" : "text-black"
+          }
+        >
           About
         </NavLink>
-        <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
+        <NavLink
+          to="/projects"
+          className={({ isActive }) =>
+            isActive ? "text-blue-600" : "text-black"
+          }
+        >
           Projects
         </NavLink>
-         <NavLink to='/resume' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
-          Resume
+        <NavLink
+          to="https://t.me/Cyber_0719"
+          className={({ isActive }) =>
+            isActive ? "text-blue-600" : "text-black"
+          }
+        >
+          Rezyume
         </NavLink>
       </nav>
     </header>
