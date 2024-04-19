db806a96622b5d5842bc5f3f4971d5e33f083f43
Madaminov Muhammadyusuf <116722541+Darkned07@users.noreply.github.com>
2024-04-19 17:53:29 +0500

    Update Resume.jsx

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,1 +1,19 @@
 
+
+import "react-vertical-timeline-component/style.min.css";
+
+const About = () => {
+  return (
+    <section className="max-container">
+     
+
+    
+
+     
+
+      
+    </section>
+  );
+};
+
+export default About;
